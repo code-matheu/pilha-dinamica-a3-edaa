719ac6e6a68f5ad9e9b2f514204a856e78c378a0
Matheus Bernardo <matheus008bernardo@gmail.com>
2025-11-26 09:41:40 -0300

    Adicionado comentarios com notação big O

--- main.py
+++ main.py
@@ -1,66 +1,66 @@
 class No:
-    def __init__(self, dado):
+    def __init__(self, dado): # O(1)
         self.dado = dado
         self.proximo = None
 
  # Implementa uma estrutura de dados fundamental de lista encadeada, insertion sort
  # usada para guardar o histórico de estados do texto. Cada vez que você altera algo, o estado anterior é "empilhado".
 class Pilha:
-    def __init__(self):
+    def __init__(self): # O(1)
         self.topo = None
 
-    def esta_vazia(self):
+    def esta_vazia(self): # O(1)
         return self.topo is None
 
-    def push(self, dado):
+    def push(self, dado): # O(1)
         novo = No(dado)
         novo.proximo = self.topo
         self.topo = novo
 
-    def pop(self):
+    def pop(self): # O(1)
         if self.esta_vazia():
             return None
         dado = self.topo.dado
         self.topo = self.topo.proximo
         return dado
 
-    def limpar(self):
+    def limpar(self): # O(1)
         self.topo = None
 
 # cérebro do editor
 class EditorTexto:
-    def __init__(self):
+    def __init__(self): # O(1)
         self.texto = ""
         self.undo = Pilha()
         self.redo = Pilha()
 
-    def escrever(self, texto):
+    def escrever(self, texto): # O(1)
         # salva estado atual antes de alterar
         self.undo.push(self.texto)
         self.texto += texto
 
-        # Toda nova ação limpa o redo
-        self.redo.limpar()
+        # Toda nova ação limpa o redo 
+        self.redo.limpar() # O(1)
 
-    def apagar(self, n):
+    def apagar(self, n): # O(1)
         self.undo.push(self.texto)
         self.texto = self.texto[:-n]
         self.redo.limpar()
 
-    def desfazer(self):
+    def desfazer(self): # O(1)
         estado_anterior = self.undo.pop()
         if estado_anterior is not None:
             self.redo.push(self.texto)
             self.texto = estado_anterior
 
-    def refazer(self):
+    def refazer(self): # O(1)
         estado = self.redo.pop()
         if estado is not None:
             self.undo.push(self.texto)
             self.texto = estado
 
-    def mostrar(self):
+    def mostrar(self): # O(1)
         return self.texto
 
 
-editor = EditorTexto()
+editor = EditorTexto() # O(1)
